49fd67bcabb0f56f2e39d1b635ada304bd05eff3
Nishant Chopde <nishantchopde2004@gmail.com>
2025-09-14 15:44:29 +0530

    Fixed cors bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ async function start() {
 
   // middleware
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || 'http://localhost:173',
     credentials: true,
   }));
   app.use(express.json());
